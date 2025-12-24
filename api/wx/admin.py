a0a91426b4b192991b22e9ef8d7d5b46114bd63b
JerryHamm <jeremiahhammond14@gmail.com>
2025-12-23 19:37:23 -0500

    updated the equipment inventory screen to have selectable locations and models...cleaned up UI...optimized query speeds

--- api/wx/admin.py
+++ api/wx/admin.py
@@ -172,7 +172,7 @@ class NoaaTransmissionTypeAdmin(admin.ModelAdmin):
 @admin.register(models.NoaaTransmissionRate)
 class NoaaTransmissionRateAdmin(admin.ModelAdmin):
     list_display = ("rate",)
-    search_fields = ("rate")
+    search_fields = ("rate",)
 
 
 @admin.register(models.NoaaDcp)
@@ -449,12 +449,12 @@ class ElementDecoder(admin.ModelAdmin):
 @admin.register(models.VisitType)
 class VisitTypeAdmin(admin.ModelAdmin):
     list_display = ("name",)
-    search_fields = ("name")
+    search_fields = ("name",)
 
 @admin.register(models.Technician)
 class TechnicianAdmin(admin.ModelAdmin):
     list_display = ("name",)
-    search_fields = ("name")
+    search_fields = ("name",)
 
 
 @admin.register(models.Manufacturer)
@@ -493,8 +493,8 @@ class EquipmentAdmin(SimpleHistoryAdmin):
             return format_html(fields)
         return None
     
-    list_display = ("equipment_type", "manufacturer", "model", "serial_number", "acquisition_date", "first_deploy_date", "last_calibration_date")
-    search_fields = ("equipment_type", "manufacturer", "model", "serial_number", "acquisition_date", "first_deploy_date", "last_calibration_date")
+    list_display = ("equipment_type", "manufacturer", "model", "serial_number", "acquisition_date", "first_deploy_date", "last_calibration_date", "location",)
+    search_fields = ("equipment_type__name", "manufacturer__name", "model__name", "serial_number", "location__name")
     history_list_display = ["changed_fields","list_changes"]
 
     def save_model(self, request, obj, form, change):
@@ -509,9 +509,14 @@ class StationProfileEquipmentTypeAdmin(admin.ModelAdmin):
 @admin.register(models.Crop)
 class CropAdmin(admin.ModelAdmin):
     list_display = ("name",)
-    search_fields = ("name")
+    search_fields = ("name",)
 
 # @admin.register(models.Soil)
 # class SoilAdmin(admin.ModelAdmin):
 #     list_display = ("soil_type",)
+
+@admin.register(models.EquipmentModel)
+class EquipmentModelAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
     
